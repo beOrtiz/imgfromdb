148836740125ba25c21e7dd591f52f239e68f298
Beatriz F Ortiz <bib_ortiz@hotmail.com>
2020-06-01 12:00:34 -0400

    Unnecessary line removed

--- index.js
+++ index.js
@@ -16,9 +16,6 @@ sql.connect(config, function(err) {
     let sqlQuery = 'Select * from tb_teste';
     sqlRequest.query(sqlQuery, function(err, data) {
         if (err) console.log(err);
-
-        const buffer = data.recordset[0].nomeDaColunaComAImagem;
-
         data.recordset.forEach(element => {
             fs.writeFileSync(`./imagens/${'NomeDoArquivo'}.pdf`, element.nomeDaColunaComAImagem);
         })
